dc17f75652e405749b607d2def16e34ec96f1178
brunaCFreitas <bruna_c_freitas@hotmail.com>
2023-05-31 11:46:23 -0300

    feat: configura variaveis be

--- app/backend/src/database/config/database.js
+++ app/backend/src/database/config/database.js
@@ -3,7 +3,7 @@ exports.__esModule = true;
 var config = {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '123456',
-    database: 'RECIPES_APP',
+    database: process.env.DB_NAME || 'RECIPES_APP',
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 3306,
     dialect: 'mysql',
